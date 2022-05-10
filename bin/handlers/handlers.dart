3d3794a816a42b23d60ca7d9e8f0a526cd753c22
Nahianether <nahianethr3@gmail.com>
2022-05-10 08:27:15 +0600

    ...

--- bin/handlers/handlers.dart
+++ bin/handlers/handlers.dart
@@ -1,11 +1,11 @@
 import 'package:shelf_router/shelf_router.dart' show Router;
 
-// import 'all_api/get_all_category_list.dart';
-// import 'all_api/get_all_clients.dart';
-// import 'all_api/get_all_iteam_list.dart';
-// import 'all_api/get_all_tables.dart';
+import 'all_api/get_all_category_list.dart';
+import 'all_api/get_all_clients.dart';
+import 'all_api/get_all_iteam_list.dart';
+import 'all_api/get_all_tables.dart';
 import 'all_api/get_token_number.dart';
-// import 'all_api/get_voucher_serial_number.dart';
+ import 'all_api/get_voucher_serial_number.dart';
 import 'all_api/post_signup.dart';
 import 'all_api/post_signin.dart';
 // import 'auth/signin.dart' show signInHandler;
@@ -20,12 +20,12 @@ final handlers = Router()
   // ..post('/sendNotificaion', sendNotificationHandler)
   // ..get('/getNotificaion', getNotificationHandler)
   // ..get('/userList', getUserListHandler)
-  // ..get('/api/v1/tables', getAllTableListHandler)
-  // ..get('/api/v1/clients', getAllClientsListHandler)
-  // ..get('/api/v1/items/categories', getAllCategoryListHandler)
-  // ..get('/api/v1/items', getAllIteamListHandler)
-  // ..get('/api/v1/voucher/serial', getVoucherSerialNumberHandler)
-  // ..get('/api/v1/orders/token', getTokenNumberHandler)
+  ..get('/api/v1/tables', getAllTableListHandler)
+  ..get('/api/v1/clients', getAllClientsListHandler)
+  ..get('/api/v1/items/categories', getAllCategoryListHandler)
+  ..get('/api/v1/items', getAllIteamListHandler)
+  ..get('/api/v1/voucher/serial', getVoucherSerialNumberHandler)
+  ..get('/api/v1/orders/token', getTokenNumberHandler)
   ..post('/api/v1/login', postSignInHandler)
   ..post('/api/v1/register', postSignUpHandler);
   //..post('/api/v1/orders/draft', getTokenNumberHandler)
